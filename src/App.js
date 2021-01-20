d6e90b69f72e1054c8c05eb4d0910fa4babf70b9
Igniteh <n0majo00@students.oamk.fi>
2021-01-20 10:30:45 +0200

    Finished example.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [tulos, Tulostus] = useState("");
+
+function laskuri(e) {
+  e.preventDefault();
+  const laskuri = weight / (height * height);
+  let lol = laskuri.toFixed(1);
+  Tulostus(lol);
+}
+
+return (
+  <div className='container'>
+    <h3>Weight calculator</h3>
+      <form onSubmit={laskuri}>
+        <div>
+          <label>Height</label>
+          <input type="number" value={height} onChange={e => setHeight(e.target.value)}></input>
+        </div>
+        <div>
+        <label>Weight</label>
+          <input type="number" value={weight} onChange={e => setWeight(e.target.value)}></input>
+        </div>
+          <div>
+          <output>{tulos}</output>
+          </div>
+        <button>Calcuate</button>
+      </form>
+  </div>
+);
 }
 
 export default App;
